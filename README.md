6b5537b64fe88346932a5dead6fe3a21efa561ef
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-12-06 12:14:54 -0700

    more initial stuff

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mft-presidential-debates
+For a paper for one of my humanities classes, *Moral Psychology and Religion*, I'm tracking how [Jonathon Haidt](https://jonathanhaidt.com)'s [Moral Foundations Theory](https://moralfoundations.org) appears in the history of presidential debates.
+
+## Disclaimers
+No political statements or claims are meant to be made via this work; this is a program being used as a research mechanism in order to automate an otherwise tedious process.
+
+The Moral Foundations dictionary (`dictionary.txt` in the folder structure) comes directly from the [Moral Foundations site](https://moralfoundations.org/wp-content/uploads/files/downloads/moral%20foundations%20dictionary.dic). This file includes potentially strong, outdated, inappropriate or uncomfortable language that does not represent my beliefs and is used here solely for scientific purposes.
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+import pprint
+
 # Presidential Debate-Moral Foundations Theory Cleaner
 CATEGORIES = {
     '01': 'HarmVirtue',
@@ -13,14 +16,36 @@ CATEGORIES = {
     '11': 'MoralityGeneral'
 }
 
+# stores word stems -> moral foundations
+mft_dictionary = {}
+
+# stores moral foundations -> appearance count
+mft_counts = {}
+
+with open('dictionary.txt') as f:
+    for line in f:
+        line: str = line.strip().split()
+        if len(line) > 0:
+            if len(line) == 2:
+                # 1:1 mapping of word to mft
+                mft_dictionary.update({ line[0]: CATEGORIES[line[1]] })
+            else:
+                # 1:many mapping of word to mfts
+                mft_dictionary.update({ line[0]: ", ".join([CATEGORIES[mapping] for mapping in line[1:]])})
+
 speech = input("Enter a speech file: ")
 
 with open(speech) as speech_file:
     for line in speech_file:
-        print(line)
+        for category, foundation in CATEGORIES.items():
+            word_count = 0
+            for stem, mfoundation in mft_dictionary.items():
+                if foundation == mfoundation:
+                    regex = re.compile(stem.replace('*', '+'))
+                    match = re.findall(regex, line)  
+                    if match != None and len(match) != 0:  
+                        print("MATCH:", match)
+                        word_count += len(match) # TODO should it be 1? this will have duplicates
+            mft_counts.update({ foundation: word_count })
 
-    with open('dict.txt') as f:
-        for line in f:
-            line: str = line.strip().split()
-            if len(line) > 0:
-                print(line)
+pprint.pprint(mft_counts)
